195eb76187d990986369188bff2c36874d98f926
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2021-11-14 16:41:46 +0530

    Update must_join.py

--- StringSessionBot/must_join.py
+++ StringSessionBot/must_join.py
@@ -19,14 +19,14 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"You must join [this channel]({link}) to use me. After joining try again !",
+                    f"A hmasa in [He Channell]({link}) hi min hmang thei thr chuan i join phawt a ngai, ti chuan i rawn ti tha leh dawn nia !",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
+                        [InlineKeyboardButton("✨Channel hi join rawh✨", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat : {MUST_JOIN} !")
+        print(f"Join na tur MUST_JOIN ah hian admin ka ni lo tlat : {MUST_JOIN} !")
